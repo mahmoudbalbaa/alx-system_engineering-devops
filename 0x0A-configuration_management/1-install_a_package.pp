e43adad21a5ac0c0ca0969705f14138fa3ae2b20
Mahmoud Balbaa <138934815+mahmoudbalbaa@users.noreply.github.com>
2024-02-24 02:00:03 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
 # install flask -v 2.1.0
-
-class flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install-flask':
-    command     => '/usr/bin/pip3 install flask==2.1.0',
-    require     => Package['python3-pip'],
-    path        => ['/bin', '/usr/bin'],
-    provider    => shell,
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-include flask
